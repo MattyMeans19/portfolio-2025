a5071fe0a86d5b3047c2227539534fba202201c2
Matthew Means <matty_means@outlook.com>
2025-12-10 10:42:22 -0700

    Update Misc.tsx
    changed layout sizing

--- components/enterprise/Misc.tsx
+++ components/enterprise/Misc.tsx
@@ -24,7 +24,7 @@ export default function Misc(props: Display){
             </div>
                 
             <div className="grow bg-white w-[80%] h-[80%] border-10 border-double border-(--tertiary) rounded-4xl my-10 place-self-center col-span-7 place-items-center 
-                p-15 flex flex-col gap-15 justify-around boxShadow">
+                p-15 flex flex-col gap-5 justify-around boxShadow">
                 <h1 className="text-4xl">{data[0].title}</h1>
                 <p className="md:text-2xl">{data[0].description}</p>
                 <h2 className="text-2xl text-center"><strong className={`${data[0].exampleSite != "" ? 'boldText-primary' : 'hidden'}`}>Example site:</strong> {data[0].exampleSite}</h2>
