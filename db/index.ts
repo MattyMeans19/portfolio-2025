a54054a8b4096bcd5b7180e8e6983415d4800ef2
Matt <mattymeans@gmail.com>
2026-03-18 00:48:27 -0600

    fixed db ssl

--- db/index.ts
+++ db/index.ts
@@ -8,7 +8,7 @@ const globalForDb = global as unknown as { pool: Pool };
 const pool = globalForDb.pool || new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" 
-    ? { rejectUnauthorized: true } 
+    ? { rejectUnauthorized: false } 
     : true,
 });
 
